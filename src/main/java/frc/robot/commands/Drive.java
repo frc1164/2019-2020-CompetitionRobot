f091b966babf0605e719aef9eb7159a1b66f91f6
Jessey Pfeiffer <jpfeiffer@ProjectNEO4.projectneo.net>
2020-03-04 20:16:39 -0700

    PIXY PID

--- src/main/java/frc/robot/commands/Drive.java
+++ src/main/java/frc/robot/commands/Drive.java
@@ -41,8 +41,8 @@ public class Drive extends CommandBase {
     double leftMSpeed = (-Math.abs(scalar)*(forward - turn));
     double rightMSpeed = (-Math.abs(scalar)*(forward + turn));
 
-    m_Chassis.leftSpeed(leftMSpeed + CenterGoal.PIDout);
-    m_Chassis.rightSpeed(rightMSpeed - CenterGoal.PIDout);
+    m_Chassis.leftSpeed(leftMSpeed + CenterGoal.PIDout + SeekBall.PIDout);
+    m_Chassis.rightSpeed(rightMSpeed - CenterGoal.PIDout - SeekBall.PIDout);
   }
 
   // Called once the command ends or is interrupted.
